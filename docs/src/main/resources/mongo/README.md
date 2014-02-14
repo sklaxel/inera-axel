0f7c0f21bc7e807df8c0d81c77d9833f56a3614d
Ekkehart Lotzsch <ekkehart.lotzsch@r2m.se>
2014-02-14 18:34:30 +0100

    AXEL-173 Move Mongo index creation to project docs

---
+++ docs/src/main/resources/mongo/README.md
@@ -0,0 +1,21 @@
+
+# Index Creation i Mongo databasen
+
+Detta är script som skapar indexer i Mongo databasen.
+
+## Filer
+
+* `createIndexes.sh`
+
+    Kör createIndexes.js mot Mongo databasen. 
+
+* `createIndexes.js`
+
+    Raderar och skapar indexerna på nytt i shsMessageEntry collection.
+
+## Köra
+
+Starta index creation i konsolen:
+
+    $ ${axel.home}/docs/mongo/createIndexes.sh
+
---
+++ docs/src/main/resources/mongo/createIndexes.js
@@ -0,0 +1,91 @@
+/*
+ * Copyright (C) 2013 Inera AB (http://www.inera.se)
+ *
+ * This file is part of Inera Axel (http://code.google.com/p/inera-axel).
+ *
+ * Inera Axel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Inera Axel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+print('------------------------------ START ------------------------------');
+// ============================================================
+// Preparation
+// ============================================================
+db = db.getSiblingDB('axel');
+print('Drop indexes');
+db.shsMessageEntry.dropIndexes();
+
+// ============================================================
+// Create indexes
+// ============================================================
+print('Create indexes');
+
+// ------------------------------------------------------------
+// Indexes for MongoMessageLogService
+// ------------------------------------------------------------
+// ............................................................
+// messageLogRepository.find...()
+// ............................................................
+// !!!
+db.shsMessageEntry.ensureIndex( { "label.txId" : 1 } );
+
+// ............................................................
+// mongoTemplate.find...()
+// ............................................................
+// !!!
+db.shsMessageEntry.ensureIndex( { "label.corrId" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.txId" : 1 } );
+
+// releaseStaleFetchingInProgress
+db.shsMessageEntry.ensureIndex( { "state" : 1, "stateTimeStamp" : 1 } );
+
+// ............................................................
+// listMessages()
+// ............................................................
+// 3 levels + sort
+// !!!
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1, "label.transferType" : 1, "state" : 1, "stateTimeStamp" : 1 } );
+
+// 4 levels + sort
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1, "label.transferType" : 1, "state" : 1, "label.product.value" : 1, "stateTimeStamp" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1, "label.transferType" : 1, "state" : 1, "acknowledged" : 1, "stateTimeStamp" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1, "label.transferType" : 1, "state" : 1, "label.status" : 1, "stateTimeStamp" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1, "label.transferType" : 1, "state" : 1, "label.endRecipient.value" : 1, "stateTimeStamp" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1, "label.transferType" : 1, "state" : 1, "label.originatorOrFrom.value" : 1, "stateTimeStamp" : 1 } );
+// !!!
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1, "label.transferType" : 1, "state" : 1, "label.corrId" : 1, "stateTimeStamp" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1, "label.transferType" : 1, "state" : 1, "label.content.contentId" : 1, "stateTimeStamp" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1, "label.transferType" : 1, "state" : 1, "label.meta.name" : 1, "stateTimeStamp" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1, "label.transferType" : 1, "state" : 1, "label.meta.value" : 1, "stateTimeStamp" : 1 } );
+
+// 5 levels + sort
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1, "label.transferType" : 1, "state" : 1, "label.product.value" : 1, "acknowledged" : 1, "stateTimeStamp" : 1 } );
+
+// ............................................................
+// TODO - indexes for archiving
+// ............................................................
+// db.shsMessageEntry.ensureIndex( { "archived" : 1, "stateTimeStamp" : 1 } );
+// db.shsMessageEntry.ensureIndex( { "stateTimeStamp" : 1, "state" : 1 } );
+
+// ------------------------------------------------------------
+// Indexes for MongoMessageLogAdminService
+// ------------------------------------------------------------
+db.shsMessageEntry.ensureIndex( { "label.to.value" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.originatorOrFrom.value" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.txId" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.corrId" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.content.dataOrCompound.filename" : 1 } );
+db.shsMessageEntry.ensureIndex( { "label.product.value" : 1 } );
+db.shsMessageEntry.ensureIndex( { "acknowledged" : 1 } );
+db.shsMessageEntry.ensureIndex( { "state" : 1 } );
+print('------------------------------ END ------------------------------');
